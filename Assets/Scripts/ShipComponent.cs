453cca0c57eeab557149ad9a4efb0c4bd09e70b0
Koje3 <91778162+Koje3@users.noreply.github.com>
2022-02-11 15:54:00 +0200

    Developed event system further

--- Assets/Scripts/ShipComponent.cs
+++ Assets/Scripts/ShipComponent.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Events;
 
 
 
@@ -9,7 +11,7 @@ public class ShipComponent : MonoBehaviour
 
     public PuzzleComponent puzzleComponent;
 
-    public List<CrisisSubType> crisisSubTypes;
+    public List<CrisisTypes> crisisTypes;
 
 
     void Start()
@@ -21,22 +23,35 @@ public class ShipComponent : MonoBehaviour
 
 
 
-
-    public void GetCrisisStateChange(PuzzleComponent crisis, CrisisSubType newCrisisType, bool isCrisisFixed)
+    //Get notice from current EventManager that crisis state has changed
+    public void GetCrisisStateChange(PuzzleComponent crisisComponent, CrisisSubType newCrisisType, bool isCrisisFixed)
     {
-        if (crisis == puzzleComponent)
+        //Check that crisis is related to this gameobject
+        if (crisisComponent == puzzleComponent)
         {
-            if (crisisSubTypes.Contains(newCrisisType))
-            {
 
+            //Check that there are actions assigned for this crisistype
+            foreach (CrisisTypes crisisType in crisisTypes)
+            {
+                if (crisisType.crisisType == newCrisisType)
+                {
+                    // Start or Stop actions related to this crisis
+                    if (isCrisisFixed == false)
+                    {
+                        StartCrisis(crisisType);
+                    }
+                    else
+                    {
+                        StopCrisis(crisisType);
+                    }
+                }
             }
-            // Change vfxState = CrisisSubType
-            // vfxstateChange activates objects, particle effects, buttons, levers etc...
 
         }
 
     }
 
+    //Send Event manager info that crisis is fixed
     public void FixCrisis(CrisisSubType crisisSubType)
     {
 
@@ -44,6 +59,8 @@ public class ShipComponent : MonoBehaviour
         
     }
 
+
+    // Ask which crises are set in current piece and which are fixed
     public void GetCrisisStates(CrisisSubType crisisSubType)
     {
         if (LevelManager.instance.currentPiece.GetCrisisState(puzzleComponent, crisisSubType))
@@ -58,5 +75,32 @@ public class ShipComponent : MonoBehaviour
         }
     }
 
+
+    // Activate crisis actions
+    public void StartCrisis(CrisisTypes startCrisisType)
+    {
+        startCrisisType.onCrisisStartEvent.Invoke();
+    }
+
+    // Stop crisis actions
+    public void StopCrisis(CrisisTypes stopCrisisType)
+    {
+        stopCrisisType.onCrisisStopEvent.Invoke();
+        
+    }
+
+    
+
 }
+
+[Serializable]
+public class CrisisTypes
+{
+    [Header("Select crisis type")]
+    public CrisisSubType crisisType;
+
+    public UnityEvent onCrisisStartEvent;
+    public UnityEvent onCrisisStopEvent;
+}
+
 
