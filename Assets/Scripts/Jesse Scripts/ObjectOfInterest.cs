56f25ff05c0e3f582ff1d4cad0b615771a16d698
Koje3 <91778162+Koje3@users.noreply.github.com>
2022-03-16 17:15:12 +0200

    Added new environment and ship to scene

---
+++ Assets/Scripts/Jesse Scripts/ObjectOfInterest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOfInterest : MonoBehaviour
+{
+
+    public Transform lookTarget;
+
+    public Transform GetLookTarget()
+    {
+        if(lookTarget != null)
+        {
+            return lookTarget;
+        }
+        else
+        {
+            return transform;
+        }
+    }
+
+}
