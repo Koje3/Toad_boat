c3d226c4401434a1b032f30a9ab375790386d7f6
Koje3 <91778162+Koje3@users.noreply.github.com>
2022-04-05 15:37:01 +0300

    Coffee cup empties

--- Assets/_Scripts/LeoScripts/DrinkingVessel.cs
+++ Assets/_Scripts/LeoScripts/DrinkingVessel.cs
@@ -2,35 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DrinkingVessel : MonoBehaviour {
-    [SerializeField] private bool isTilted;
+public class DrinkingVessel : MonoBehaviour
+{
+    [SerializeField] private bool isTilted = false;
+    [SerializeField] private bool isEmpty = false;
     public ParticleSystem pouringEffect;
+    public GameObject coffee;
 
-    // Update is called once per frame
-    void Update() {
+    public float timeRemaining;
+    void Update()
+    {
         RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, Mathf.Infinity)) {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, Mathf.Infinity))
+        {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * hit.distance, Color.yellow);
-            if (hit.collider.CompareTag("TiltCollider")) {
-                if (isTilted == false) {
-                    isTilted = true;
-                    TiltStarted();
+            if (hit.collider.CompareTag("TiltCollider"))
+            {
+                if (isTilted == false)
+                {
+                    if (isEmpty == false)
+                    {
+                        TiltStarted();
+                    }
                 }
-            } else {
-                if (isTilted == true) {
-                    isTilted = false;
+            }
+            else
+            {
+                if (isTilted == true)
+                {
                     TiltEnded();
                 }
             }
+        }        
+        if (isTilted == true)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 0f;
+                VesselEmpty();
+            }
         }
     }
-    void TiltStarted() {
+    void TiltStarted()
+    {       
+        isTilted = true;
+
         var emission = pouringEffect.emission;
         emission.rateOverTime = 20f;
     }
 
-    void TiltEnded() {
+    void TiltEnded()
+    {
+        isTilted = false;
+
+        var emission = pouringEffect.emission;
+        emission.rateOverTime = 0f;
+    }
+
+    void VesselEmpty()
+    {
+        isEmpty = true;
+        coffee.SetActive(false);
         var emission = pouringEffect.emission;
         emission.rateOverTime = 0f;
     }
