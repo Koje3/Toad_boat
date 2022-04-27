030c8d2528d98b0c8b88f42e877dda0df326ee7a
Koje3 <91778162+Koje3@users.noreply.github.com>
2022-04-27 13:22:56 +0300

    Lever puzzle logic (WIP)

--- Assets/_Scripts/Jesse Scripts/LeverPuzzle2.cs
+++ Assets/_Scripts/Jesse Scripts/LeverPuzzle2.cs
@@ -13,6 +13,7 @@ public class LeverPuzzle2 : MonoBehaviour
     public CrisisSubType crisisTypeToSolve;
 
     public bool puzzleOpen = false;
+    public bool firstSuccessLoopCompleted = false;
 
     public List<LeverInfo> levers;
 
@@ -21,8 +22,8 @@ public class LeverPuzzle2 : MonoBehaviour
     void Start()
     {
         foreach (LeverInfo item in levers)
-        {            
-            item.lever.onLeverChange.AddListener(LeverValueChanged);            
+        {
+            item.lever.onLeverChange.AddListener(LeverValueChanged);
             item.indicator = item.lever.gameObject.transform.parent.Find("LeverIndicator").gameObject;
             item.solvedValue = UnityEngine.Random.Range(0f, 100.0f);
         }
@@ -54,7 +55,7 @@ public class LeverPuzzle2 : MonoBehaviour
             if (item.currentLeverValue < item.solvedValue - 1 || item.currentLeverValue > item.solvedValue + 1)
             {
                 Debug.Log("One or more levers are incorrect position");
-                Debug.Log( item.lever.gameObject.transform.parent.name + " is incorrect position");
+                Debug.Log(item.lever.gameObject.transform.parent.name + " is incorrect position");
 
                 return false;
             }
@@ -79,12 +80,18 @@ public class LeverPuzzle2 : MonoBehaviour
                     if (item.indicator != null)
                         item.indicator.GetComponent<Renderer>().material = indicatorMaterialCorrect;
 
-                    if (item.leverCorrectAction != null)
-                        item.leverCorrectAction.Invoke();
-
+                    if (firstSuccessLoopCompleted == false)
+                    {
+                        if (item.leverCorrectAction != null)
+                        {
+                            item.leverCorrectAction.Invoke();
+                            firstSuccessLoopCompleted = true;
+                        }
+                    }
                 }
                 else
                 {
+                    firstSuccessLoopCompleted = false;
                     if (item.indicator != null)
                         item.indicator.GetComponent<Renderer>().material = indicatorMaterialIncorrect;
 
@@ -93,7 +100,7 @@ public class LeverPuzzle2 : MonoBehaviour
                 }
             }
         }
-         
+
     }
 
     public void ResetPuzzle()
@@ -114,9 +121,9 @@ public class LeverPuzzle2 : MonoBehaviour
 public class LeverInfo
 {
     [Header("Select object with 'Lever' -script component")]
-    [Space (2)]
+    [Space(2)]
     public BNG.Lever lever;
-    [Space (5)]
+    [Space(5)]
     public float solvedValue;
     [Space(5)]
     public float currentLeverValue;
